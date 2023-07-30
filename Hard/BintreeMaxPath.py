00be18e34201ab901f55fbd27e19412f81151cea
Didrik Molinder <didrik.molinder@protonmail.com>
2023-07-30 21:25:02 +0200

    solved edge case of all negatives

--- Hard/BintreeMaxPath.py
+++ Hard/BintreeMaxPath.py
@@ -1,23 +1,38 @@
 #https://1337code.xyz/hard/binary-tree-maximum-path-sum
 
 class Node():
-    def __init__(self, value):
+
+    def __init__(self, value, binList):
         self.value = value
         self.left_child = None
         self.right_child = None
+        self.binList = binList
         
 def binTree(binList, i=0):
+    
     if i < len(binList):
         value = binList[i]
         if value == None:
             return
-        node = Node(value)
+        node = Node(value, binList)
         node.left_child = binTree(binList, i * 2 + 1)
         node.right_child = binTree(binList, i * 2 + 2)
         return node
-    return
 
 def pathFinder(root):
+
+    """edge case where all nodes are less than zero"""
+    try:
+        neg_max = float("-inf")
+        for x in root.binList:
+            if x != None:
+                neg_max = max(neg_max, x)
+        if neg_max < 0 and neg_max != float("-inf"):
+            return neg_max
+    except:
+        pass
+
+    """main algorithm"""
     try:
         left_sum = max(0, pathFinder(root.left_child))
         right_sum = max(0, pathFinder(root.right_child))
@@ -26,7 +41,8 @@ def pathFinder(root):
     except:
         return 0
 
-print(pathFinder(binTree([1,2,3])))
-print(pathFinder(binTree([-10,9,20,None,None,15,7])))
-print(pathFinder(binTree([-10,-9,20,None,None,15,-7])))
-print(pathFinder(binTree([-10,-9,-20,None,None,15,-7,None,None,None,None,5])))
+print("expected 6, output:",pathFinder(binTree([1,2,3])))
+print("expected 42, output:",pathFinder(binTree([-10,9,20,None,None,15,7])))
+print("expected 35, output:",pathFinder(binTree([-10,-9,20,None,None,15,-7])))
+print("expected 20, output:",pathFinder(binTree([-10,-9,-20,None,None,15,-7,None,None,None,None,5])))
+print("expected -1, output:",pathFinder(binTree([-1,-2,-3])))
